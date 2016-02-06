3093fc14a7f010e8ce583f786b021310df91e15c
Sami Haahtinen <sami.haahtinen@hakosalo.fi>
2016-02-06 12:12:11 +0200

    Add a default config template

--- 0.11/entrypoint.sh
+++ 0.11/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+# If we are called with default arguments, invoke SickGear with datadir and
+# setup the environment
 if [ "$1" = 'SickBeard.py' ]; then
-    chown -R ${APP_UID:=0}:${APP_GID:=0} "$APP_DATA"
+  if [ ! -f ${APP_DATA}/config.ini ]; then
+    cp /template/config.ini ${APP_DATA}/
+  fi
+  chown -R ${APP_UID:=0}:${APP_GID:=0} "$APP_DATA"
 
-    exec gosu $APP_UID:$APP_GID python "$@" --datadir=$APP_DATA
+  exec gosu $APP_UID:$APP_GID python "$@" --datadir=$APP_DATA
 fi
 
 exec "$@"

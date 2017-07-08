dbe5c17d00fc754140dc81556179ac94a5c5b325
George Hafiz <george@hafiz.uk>
2017-07-08 21:57:59 +0100

    Add support for NFS backed config directories, where we must impersonate the uid when creating files, and we are unable to chown files, so we check if the correct permissions are set before attempting an operation (chown) that will fail.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,17 @@ set -e
 # setup the environment
 if [ "$1" = 'SickBeard.py' ]; then
   if [ ! -f ${APP_DATA}/config.ini ]; then
-    cp /template/config.ini ${APP_DATA}/
+    exec gosu ${APP_UID:=0}:${APP_GID:=0} cp /template/config.ini ${APP_DATA}/
   fi
-  chown -R ${APP_UID:=0}:${APP_GID:=0} "$APP_DATA"
+  CFG_DIR_UID=`ls -lnd "$APP_DATA" | awk 'NR==1 {print $3}'`
+  CFG_DIR_GID=`ls -lnd "$APP_DATA" | awk 'NR==1 {print $4}'`
+  if [ $APP_UID -ne $CFG_DIR_UID ] && [ $APP_GID -ne $CFG_DIR_GID ]; then
+    chown -R ${APP_UID:=0}:${APP_GID:=0} "$APP_DATA"
+  fi
 
   cd /opt/SickGear
 
-  exec gosu $APP_UID:$APP_GID python "$@" --datadir=$APP_DATA
+  exec gosu ${APP_UID:=0}:${APP_GID:=0} python "$@" --datadir=$APP_DATA
 fi
 
 exec "$@"

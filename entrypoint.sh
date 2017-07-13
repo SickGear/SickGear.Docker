f6ee8c7cc88c00efb4d1e387c5e8a381fb326de8
George Hafiz <george@hafiz.uk>
2017-07-13 20:01:09 +0100

    Use stat instead of ls parsing. Refactor uid and gid referencing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,17 @@ set -e
 # If we are called with default arguments, invoke SickGear with datadir and
 # setup the environment
 if [ "$1" = 'SickBeard.py' ]; then
+  APP_DATA_UIDGID="${APP_UID:=0}:${APP_GID:=0}"
   if [ ! -f ${APP_DATA}/config.ini ]; then
-    exec gosu ${APP_UID:=0}:${APP_GID:=0} cp /template/config.ini ${APP_DATA}/
+    exec gosu ${APP_DATA_UIDGID} cp /template/config.ini ${APP_DATA}/
   fi
-  CFG_DIR_UID=`ls -lnd "$APP_DATA" | awk 'NR==1 {print $3}'`
-  CFG_DIR_GID=`ls -lnd "$APP_DATA" | awk 'NR==1 {print $4}'`
-  if [ $APP_UID -ne $CFG_DIR_UID ] && [ $APP_GID -ne $CFG_DIR_GID ]; then
-    chown -R ${APP_UID:=0}:${APP_GID:=0} "$APP_DATA"
+  if [ $(stat -c "%u:%g" ${APP_DATA}) != "${APP_DATA_UIDGID}" ]; then
+    chown -R ${APP_DATA_UIDGID} "$APP_DATA"
   fi
 
   cd /opt/SickGear
 
-  exec gosu ${APP_UID:=0}:${APP_GID:=0} python "$@" --datadir=$APP_DATA
+  exec gosu ${APP_DATA_UIDGID} python "$@" --datadir=$APP_DATA
 fi
 
 exec "$@"

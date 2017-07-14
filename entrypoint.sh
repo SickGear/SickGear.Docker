c9bdb535f29aedec5110ddc5751abe0939520057
George Hafiz <george@hafiz.uk>
2017-07-14 18:07:53 +0100

    Fix to `exec` usage too early, erroneously breaking script execution flow.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 if [ "$1" = 'SickBeard.py' ]; then
   APP_DATA_UIDGID="${APP_UID:=0}:${APP_GID:=0}"
   if [ ! -f ${APP_DATA}/config.ini ]; then
-    exec gosu ${APP_DATA_UIDGID} cp /template/config.ini ${APP_DATA}/
+    gosu ${APP_DATA_UIDGID} cp /template/config.ini ${APP_DATA}/
   fi
   if [ $(stat -c "%u:%g" ${APP_DATA}) != "${APP_DATA_UIDGID}" ]; then
     chown -R ${APP_DATA_UIDGID} "$APP_DATA"
